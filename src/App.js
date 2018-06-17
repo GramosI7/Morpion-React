7f421768fd07582c4f16ca3b2150c9077a66a009
GramosI7 <imeri.gramos@gmail.com>
2018-06-17 13:47:09 +0200

    Le gagnant est X ou O ?

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
     this.state = {
       turn: "X",
       gameEnded: false,
-      board: Array(9).fill("")
+      winner: undefined,
+      board: Array(9).fill(""),
+      totalMoves: 0
     }
   }
 
@@ -23,10 +25,46 @@ class App extends Component {
       //change la valeur a chaque fois et met a jour le board
       this.setState({
         turn: this.state.turn === "X" ? "O" : "X",
-        board: this.state.board
+        board: this.state.board,
+        totalMoves: this.state.totalMoves++
       }, () => {
-        console.log(this.state.board)
+        // console.log(this.state.board)
       })
+    }
+    var result = this.checkWinner();
+    //si le tab renvoyé est 3 x "X"
+    if (result === "X") {
+      this.setState({
+        gameEnded: true,
+        winner: "X"
+      }, () => {
+        console.log(this.state.winner)
+      })
+
+      //si le tab renvoyé est 3 x "X"
+    } else if (result === "O") {
+      this.setState({
+        gameEnded: true,
+        winner: "O"
+      }, () => {
+        console.log(this.state.winner)
+      })
+
+    }
+    // console.log(this.state.gameEnded)
+  }
+
+  checkWinner = () => {
+    //toutes les combinaisons win
+    const moves = [[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6], [0, 1, 2], [3, 4, 5], [6, 7, 8]];
+    var board = this.state.board;
+    //boucle de moves = combinaisons win
+    for (let i = 0; i < moves.length; i++) {
+      //si un des tableau on trois fois la meme valeur alors return le tableau avec la combinaison win
+      if (board[moves[i][0]] === board[moves[i][1]] && board[moves[i][1]] === board[moves[i][2]]) {
+        return board[moves[i][0]];
+        // console.log("board", board[moves[i][0]])
+      }
     }
   }
 
@@ -47,6 +85,7 @@ class App extends Component {
           <div className="square" data-square="7"></div>
           <div className="square" data-square="8"></div>
         </div>
+        <h2>Le gagnant est {this.state.winner}</h2>
       </div>
     );
   }
